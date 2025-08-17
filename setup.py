daa457e96fdfb68c76895a25605fec08c3c530ed
Roberts Slisans <roberts.slisans@gmail.com>
2025-08-17 13:40:22 +0300

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
-setup()
+setup(
+    name="higgs-audio",
+    version="0.1.0",
+    description="Text-audio foundation model from Boson AI",
+    author="Boson AI",
+    author_email="your@email.com",
+    url="https://github.com/rsxdalv/higgs-audio",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
